b499041243be744df1dc7027ea7f1eedce877530
Alexandre Besse <ialexandreibesse@gmail.com>
2020-04-28 14:01:57 +0200

    :bug: (back): Fix bugs
    Correction de bugs lié à postgres

---
+++ back/migrations/deploy/table.sql
@@ -0,0 +1,179 @@
+BEGIN;
+
+CREATE TABLE "user" (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fist_name char(40) NOT NULL,
+    last_name char(40) NOT NULL,
+    "login" char(40) NOT NULL,
+    "password" char(76) NOT NULL,
+    avatar text,
+    mail text
+
+);
+
+CREATE TABLE user_profil (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    "address" text,
+    zip_code text,
+    city text,
+    phone_number text,
+    cellphone_number text,
+    phone_work text,
+    children int,
+    statut text,
+    gender text
+
+);
+
+CREATE TABLE "service" (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "category" text NOT NULL,
+    "name" text NOT NULL,
+    logo text NOT NULL,
+    link text NOT NULL,
+    color text
+
+);
+
+CREATE TABLE "article" (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    "service_id" int REFERENCES "service"(id),
+    title text NOT NULL,
+    content text NOT NULL,
+    "description" text NOT NULL,
+    "image" text,
+    updated_date date,
+    created_date date
+
+);
+
+
+CREATE TABLE info (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    identify text,
+    "service_name" text NOT NULL,
+    service_phone text,
+    service_address text,
+    service_referent text,
+    note_file text
+
+);
+
+CREATE TABLE document (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    "service_id" int REFERENCES "service"(id),
+    "name" text NOT NULL,
+    link text NOT NULL,
+    updated_date date,
+    created_date date
+
+);
+
+CREATE TABLE todolist (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    title text NOT NULL,
+    content text NOT NULL,
+    favorite boolean DEFAULT FALSE NOT NULL
+
+);
+
+CREATE TABLE note (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    title text NOT NULL,
+    content text NOT NULL,
+    favorite boolean DEFAULT FALSE NOT NULL
+
+);
+
+CREATE TABLE letter_type (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    "service_id" int REFERENCES "service"(id),
+    article_id int REFERENCES article(id),
+    title text NOT NULL,
+    content text NOT NULL
+
+);
+
+CREATE TABLE simulation (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    article_id int REFERENCES article(id),
+    "service_id" int REFERENCES "service"(id),
+    simulation_result_id int REFERENCES simulation(id),
+    title text NOT NULL
+
+);
+
+CREATE TABLE checklist (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    article_id int REFERENCES article(id),
+    simulation_id int REFERENCES simulation(id),
+    service_id int REFERENCES service(id),
+    title text NOT NULL
+
+);
+
+CREATE TABLE user_checklist (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" int REFERENCES "user"(id),
+    "service_id" int REFERENCES "service"(id),
+    title text NOT NULL
+
+);
+
+CREATE TABLE checklist_item (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    document_id int REFERENCES document(id),
+    checklist_id int REFERENCES checklist(id),
+    user_checklist_id int REFERENCES user_checklist(id),
+    item text
+
+);
+
+CREATE TABLE simulation_field (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    simulation int REFERENCES simulation(id),
+    label text NOT NULL,
+    input_type text NOT NULL,
+    obligatory boolean NOT NULL
+
+);
+
+CREATE TABLE simulation_field_select (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    simulation_field_id int REFERENCES simulation_field(id),
+    label text
+
+);
+
+CREATE TABLE simulation_result (
+
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    checklist_id int REFERENCES checklist(id),
+    title text NOT NULL,
+    content text NOT NULL
+
+);
+
+COMMIT;
---
+++ back/migrations/verify/table.sql
@@ -0,0 +1,7 @@
+-- Verify aldahe:table on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
