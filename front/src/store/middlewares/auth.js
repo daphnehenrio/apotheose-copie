8815992fd3d9aa26953b23b844fdd24d7818e76b
henri <henrilaurentoclock@gmail.com>
2020-04-29 17:57:13 +0200

    :art: (Axios): Change object sent by Axios post login request

--- front/src/store/middlewares/auth.js
+++ front/src/store/middlewares/auth.js
@@ -8,22 +8,29 @@ import {
 export default (store) => (next) => (action) => {
     switch (action.type) {
         case LOGIN: {
-            axios
-                .post('http://localhost:5050/login', {
-                    user: store.getState().user.user
-                }, {
-                    withCredentials: true,
-                })
-                .then((response) => {
-                    console.log(response);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-            return;
-        }
-        default: {
-            next(action);
-        }
-    }
+            console.log('kioukiou axios')
+            console.log(store.getState().user.user)
+            const user = store.getState().user.user;
+            const userLogin = {
+              login: user.username ,
+              password: user.password ,
+            }
+              axios
+                  .post('http://localhost:5050/login',
+                    userLogin
+                  , {
+                      withCredentials: true,
+                  })
+                  .then((response) => {
+                      console.log(response);
+                  })
+                  .catch((err) => {
+                      console.log(err);
+                  });
+              return;
+          }
+          default: {
+              next(action);
+          }
+      }
 };
