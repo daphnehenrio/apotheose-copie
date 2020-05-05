9db36e84fa4da2f9a01367abdd5a90b33f12d10c
Kaihatsusha <daphne.kaihatsusha@gmail.com>
2020-05-05 22:20:45 +0200

    :pencil2: (all): Indent

--- front/src/components/Profil/index.js
+++ front/src/components/Profil/index.js
@@ -15,22 +15,20 @@ import TextField from '@material-ui/core/TextField';
 import CloseIcon from '@material-ui/icons/Close';
 
 
-
 // == Import actions
 
 import {
 
-    actionSetOpenEditProfil,
-    actionSetOpenAddInfoSup,
-    actionAddInfoSup,
-    actionSetInfoSupTitle,
-    actionSetInfoSupValue,
-    actionClearAddInfoSup,
+  actionSetOpenEditProfil,
+  actionSetOpenAddInfoSup,
+  actionAddInfoSup,
+  actionSetInfoSupTitle,
+  actionSetInfoSupValue,
+  actionClearAddInfoSup,
 } from '../../actions/profil';
 
 // == import utils
 import { handdleVerifEmptyValue } from 'src/utils/checkSpaces';
-
 
 
 // == Import styles
@@ -100,183 +98,196 @@ const useStyles = makeStyles((theme) => ({
 // -------------------------- Export --------------------------
 
 export default function Profil() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const dispatch = useDispatch();
+  const openAddInfoSup = useSelector((state) => state.profil.openAddInfoSup);
+  const infoSup = useSelector((state) => state.profil.infoSupToAdd);
+  const infosSup = useSelector((state) => state.profil.infosSup);
+  const infosSupList = infosSup.map((info) => (
+    <li key={info.title} className="infos-content">{info.title} : {info.value}</li>
+  ));
 
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-    const dispatch = useDispatch();
-    const openAddInfoSup = useSelector((state) => state.profil.openAddInfoSup);
-    const infoSup = useSelector((state) => state.profil.infoSupToAdd);
-    const infosSup = useSelector((state) => state.profil.infosSup);
-    const infosSupList = infosSup.map(info => {
-        return (
-            <li key={info.title} className='infos-content'>{info.title} : {info.value}</li>
-        )
-    });
+  const handleEditProfil = (bool) => {
+    dispatch(actionSetOpenEditProfil(bool));
+  };
 
-    const handleEditProfil = (bool) => {
-        dispatch(actionSetOpenEditProfil(bool));
-    }
+  const handleAddInfoSup = (bool) => {
+    dispatch(actionSetOpenAddInfoSup(bool));
+  };
 
-    const handleAddInfoSup = (bool) => {
-        dispatch(actionSetOpenAddInfoSup(bool));
-    }
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
 
-    // -------------------------- Return --------------------------
+  // -------------------------- Return --------------------------
 
-    return (
-        <div className='profil'>
-            <h3 className='profil-title'>Mon Profil</h3>
-            <div className={classes.root}>
-                <Tabs
-                    orientation="vertical"
-                    variant="scrollable"
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="Vertical tabs example"
-                    className={classes.tabs}
+  return (
+    <div className="profil">
+      <h3 className="profil-title">Mon Profil</h3>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Category label="Moi" {...a11yProps(0)} />
+          <Category label="Infos supplémentaires" {...a11yProps(1)} />
+          <Category label="Santé" {...a11yProps(2)} />
+          <Category label="Caf" {...a11yProps(3)} />
+        </Tabs>
+        <TabPanel className="category-content" value={value} index={0}>
+          <div className="card-container">
+            <div className="card-header">
+              <div className="card-header-left">
+                <Avatar aria-label="recipe">
+                  H
+                </Avatar>
+                <h3>Mes Informations</h3>
+              </div>
+              <div className="card-header-right">
+                <IconButton
+                  aria-label="settings"
+                  onClick={(evt) => {
+                    handleEditProfil(true);
+                  }}
                 >
-                    <Category label="Moi" {...a11yProps(0)} />
-                    <Category label="Infos supplémentaires" {...a11yProps(1)} />
-                    <Category label="Santé" {...a11yProps(2)} />
-                    <Category label="Caf" {...a11yProps(3)} />
-                </Tabs>
-                <TabPanel className='category-content' value={value} index={0}>
-                    <div className='card-container'>
-                        <div className='card-header'>
-                            <div className='card-header-left'>
-                                <Avatar aria-label="recipe">
-                                    H
-                                </Avatar>
-                                <h3>Mes Informations</h3>
-                            </div>
-                            <div className='card-header-right'>
-                                <IconButton aria-label="settings" onClick={(evt) => { handleEditProfil(true) }}>
-                                    <EditIcon />
-                                </IconButton>
-                            </div>
-                        </div>
-                        <div className='card-content'>
-                            <ul className='infos-container'>
-                                <div className='sub-container'>
-                                    <h5>Nom et prénom :</h5>
-                                    <div className='sub-container-content'>
-                                        <li className='infos-content'>LAURENT Henri</li>
-                                    </div>
-
-                                </div>
-                                <div className='sub-container'>
-                                    <h5>Adresse :</h5>
-                                    <div className='sub-container-content'>
-                                        <li className='infos-content'>2 allée Louise Michel, 29000 QUIMPER</li>
-                                    </div>
-
-                                </div>
-                                <div className='sub-container'>
-                                    <h5>Téléphones :</h5>
-                                    <div className='sub-container-content'>
-                                        <div className='sub-container'>
-                                            <h5>Travail :</h5>
-                                            <li className='infos-content'>0651289512</li>
-                                        </div>
-                                        <div className='sub-container'>
-                                            <h5>Perso :</h5>
-                                            <li className='infos-content'>0651289512</li>
-                                        </div>
-                                        <div className='sub-container'>
-                                            <h5>Fix :</h5>
-                                            <li className='infos-content'>0651289512</li>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div className='sub-container'>
-                                    <h5>Autre :</h5>
-                                    <div className='sub-container-content'>
-                                        <li className='infos-content'>Age : 20</li>
-                                        <li className='infos-content'>Civilité : Homme</li>
-                                        <li className='infos-content'>Status : EN couple</li>
-                                    </div>
-
-                                </div>
-                            </ul>
-                        </div>
-                    </div>
-                </TabPanel>
-                <TabPanel className='category-content' value={value} index={1}>
-                    <div className='card-container'>
-                        <div className='card-header'>
-                            <div className='card-header-left'>
-                                <Avatar aria-label="recipe">
-                                    H
-                                </Avatar>
-                                <h3>Informations supplémentaires</h3>
-                            </div>
-                            <div className='card-header-right'>
-                                <IconButton aria-label="settings" onClick={(evt) => { 
-                                    handleAddInfoSup(true);
-                                    document.getElementById('add-info-sup-title').focus();
-                                    }}>
-                                    <AddIcon />
-                                </IconButton>
-                                <IconButton aria-label="settings">
-                                    <EditIcon />
-                                </IconButton>
-                            </div>
-                        </div>
-                        <div className='card-content'>
-                            <ul className='infos-container'>
-                                {infosSupList}
-                                {openAddInfoSup && (
-                                    <form onSubmit={(evt) => {
-                                        console.log('SUBMIT DU FORM');
-                                        evt.preventDefault();
-                                        if (!(handdleVerifEmptyValue(infoSup.title) || handdleVerifEmptyValue(infoSup.value))) {
-                                            dispatch(actionAddInfoSup(infoSup));
-                                            dispatch(actionClearAddInfoSup());
-                                            document.getElementById('add-info-sup-title').focus();
-                                        }
-                                    }}
-                                        className='add-infos-sup-container'>
-                                        <TextField
-                                            id="add-info-sup-title"
-                                            onChange={(evt) => { dispatch(actionSetInfoSupTitle(evt.target.value)) }}
-                                            value={infoSup.title}
-                                            autoFocus
-                                        />
-                                        <p className='add-infos-sup-separator'> : </p>
-                                        <TextField
-                                            id="standard-basic"
-                                            onChange={(evt) => { dispatch(actionSetInfoSupValue(evt.target.value)) }}
-                                            value={infoSup.value}
-                                        />
-                                        <IconButton aria-label="delete" onClick={(evt) => { handleAddInfoSup(false) }}>
-                                            <CloseIcon />
-                                        </IconButton>
-                                        <input className='hidden' type='submit' />
-                                    </form>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                </TabPanel >
-                <TabPanel className='category-content' value={value} index={2}>
-
-                </TabPanel>
-                <TabPanel className='category-content' value={value} index={3}>
-                    Item Four
-                 </TabPanel>
+                  <EditIcon />
+                </IconButton>
+              </div>
             </div>
-            <EditProfil />
+            <div className="card-content">
+              <ul className="infos-container">
+                <div className="sub-container">
+                  <h5>Nom et prénom :</h5>
+                  <div className="sub-container-content">
+                    <li className="infos-content">LAURENT Henri</li>
+                  </div>
+
+                </div>
+                <div className="sub-container">
+                  <h5>Adresse :</h5>
+                  <div className="sub-container-content">
+                    <li className="infos-content">2 allée Louise Michel, 29000 QUIMPER</li>
+                  </div>
+
+                </div>
+                <div className="sub-container">
+                  <h5>Téléphones :</h5>
+                  <div className="sub-container-content">
+                    <div className="sub-container">
+                      <h5>Travail :</h5>
+                      <li className="infos-content">0651289512</li>
+                    </div>
+                    <div className="sub-container">
+                      <h5>Perso :</h5>
+                      <li className="infos-content">0651289512</li>
+                    </div>
+                    <div className="sub-container">
+                      <h5>Fix :</h5>
+                      <li className="infos-content">0651289512</li>
+                    </div>
+                  </div>
+
+                </div>
+                <div className="sub-container">
+                  <h5>Autre :</h5>
+                  <div className="sub-container-content">
+                    <li className="infos-content">Age : 20</li>
+                    <li className="infos-content">Civilité : Homme</li>
+                    <li className="infos-content">Status : EN couple</li>
+                  </div>
+
+                </div>
+              </ul>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel className="category-content" value={value} index={1}>
+          <div className="card-container">
+            <div className="card-header">
+              <div className="card-header-left">
+                <Avatar aria-label="recipe">
+                  H
+                </Avatar>
+                <h3>Informations supplémentaires</h3>
+              </div>
+              <div className="card-header-right">
+                <IconButton
+                  aria-label="settings"
+                  onClick={(evt) => {
+                    handleAddInfoSup(true);
+                    document.getElementById('add-info-sup-title').focus();
+                  }}
+                >
+                  <AddIcon />
+                </IconButton>
+                <IconButton aria-label="settings">
+                  <EditIcon />
+                </IconButton>
+              </div>
+            </div>
+            <div className="card-content">
+              <ul className="infos-container">
+                {infosSupList}
+                {openAddInfoSup && (
+                <form
+                  onSubmit={(evt) => {
+                    console.log('SUBMIT DU FORM');
+                    evt.preventDefault();
+                    if (!(handdleVerifEmptyValue(infoSup.title) || handdleVerifEmptyValue(infoSup.value))) {
+                      dispatch(actionAddInfoSup(infoSup));
+                      dispatch(actionClearAddInfoSup());
+                      document.getElementById('add-info-sup-title').focus();
+                    }
+                  }}
+                  className="add-infos-sup-container"
+                >
+                  <TextField
+                    id="add-info-sup-title"
+                    onChange={(evt) => {
+                      dispatch(actionSetInfoSupTitle(evt.target.value));
+                    }}
+                    value={infoSup.title}
+                    autoFocus
+                  />
+                  <p className="add-infos-sup-separator"> : </p>
+                  <TextField
+                    id="standard-basic"
+                    onChange={(evt) => {
+                      dispatch(actionSetInfoSupValue(evt.target.value));
+                    }}
+                    value={infoSup.value}
+                  />
+                  <IconButton
+                    aria-label="delete"
+                    onClick={(evt) => {
+                      handleAddInfoSup(false);
+                    }}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <input className="hidden" type="submit" />
+                </form>
+                )}
+              </ul>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel className="category-content" value={value} index={2} />
+        <TabPanel className="category-content" value={value} index={3}>
+          Item Four
+        </TabPanel>
+      </div>
+      <EditProfil />
 
 
-        </div>
+    </div>
 
 
-    );
-
-};
+  );
+}
--- front/src/reducers/profil.js
+++ front/src/reducers/profil.js
@@ -1,95 +1,94 @@
 import {
 
-    SET_NOTE_CONTENT,
-    CHANGE_NOTE_CONTENT,
-    SET_OPEN_EDIT_PROFIL,
-    SET_OPEN_ADD_INFO_SUP,
-    ADD_INFO_SUP,
-    SET_INFO_SUP_TITLE,
-    SET_INFO_SUP_VALUE,
-    CLEAR_INFO_SUP,
+  SET_NOTE_CONTENT,
+  CHANGE_NOTE_CONTENT,
+  SET_OPEN_EDIT_PROFIL,
+  SET_OPEN_ADD_INFO_SUP,
+  ADD_INFO_SUP,
+  SET_INFO_SUP_TITLE,
+  SET_INFO_SUP_VALUE,
+  CLEAR_INFO_SUP,
 } from '../actions/profil';
 
 const initialState = {
-    noteContent: { id: '',title: '', content: '' },
-    notes: [
-        { id: 1, title: 'Note 1', content: 'hello, note numéro 1' },
-        { id: 2, title: 'Note 2', content: 'Je suis la note numéro 2' },
-    ],
-    openEditProfil: false,
-    openAddInfoSup: false,
-    infosSup: [
-        {title: 'Ecole', value:'Poudlard'},
-        {title: 'Nounou', value:'Joséphine'},
-    ],
-    infoSupToAdd: {title: '', value: ''},
+  noteContent: { id: '', title: '', content: '' },
+  notes: [
+    { id: 1, title: 'Note 1', content: 'hello, note numéro 1' },
+    { id: 2, title: 'Note 2', content: 'Je suis la note numéro 2' },
+  ],
+  openEditProfil: false,
+  openAddInfoSup: false,
+  infosSup: [
+    { title: 'Ecole', value: 'Poudlard' },
+    { title: 'Nounou', value: 'Joséphine' },
+  ],
+  infoSupToAdd: { title: '', value: '' },
 };
 
 export default (state = initialState, action = {}) => {
-    switch (action.type) {
-        case SET_NOTE_CONTENT: {
+  switch (action.type) {
+    case SET_NOTE_CONTENT: {
+      return {
+        ...state,
+        noteContent: { id: action.note.id, title: action.note.title, content: action.note.content },
+      };
+    }
+    case CHANGE_NOTE_CONTENT: {
+      return {
+        ...state,
+        notes: state.notes.map((note) => {
+          if (note.id === action.note.id) {
             return {
-                ...state,
-                noteContent: {id: action.note.id, title: action.note.title, content: action.note.content},
-            }
-        }
-        case CHANGE_NOTE_CONTENT: {
-            return {
-                ...state,
-                notes: state.notes.map(note => {
-                    if (note.id === action.note.id) {
-                        return {
-                            ...note,title: action.note.title, content: action.note.content
-                        }
-                    } else return {
-                        ...note,
-                    }
-                }
-                ),
-            }
-        }
-        case SET_OPEN_EDIT_PROFIL: {
-            return{
-                ...state,
-                openEditProfil: action.bool,
-            }
-        }
-        case SET_OPEN_ADD_INFO_SUP: {
-            return {
-                ...state,
-                openAddInfoSup: action.bool,
-            }
-        }
-        case SET_INFO_SUP_TITLE: {
-            return {
-                ...state,
-                infoSupToAdd: {...state.infoSupToAdd, title: action.title }
-            }
-        }
-        case SET_INFO_SUP_VALUE: {
-            return {
-                ...state,
-                infoSupToAdd: {...state.infoSupToAdd, value: action.value}
-            }
-        }
-        case ADD_INFO_SUP: {
-            return{
-                ...state,
-                infosSup: [
-                    ...state.infosSup,
-                    {title: action.info.title, value: action.info.value},
-                ],
-            }
-        }
-        case CLEAR_INFO_SUP: {
-            return {
-                ...state,
-                infoSupToAdd:{title: '', value: ''},
-            }
-        }
-        default: {
-            return state;
-        }
+              ...note, title: action.note.title, content: action.note.content,
+            };
+          }
+          return {
+            ...note,
+          };
+        }),
+      };
+    }
+    case SET_OPEN_EDIT_PROFIL: {
+      return {
+        ...state,
+        openEditProfil: action.bool,
+      };
+    }
+    case SET_OPEN_ADD_INFO_SUP: {
+      return {
+        ...state,
+        openAddInfoSup: action.bool,
+      };
+    }
+    case SET_INFO_SUP_TITLE: {
+      return {
+        ...state,
+        infoSupToAdd: { ...state.infoSupToAdd, title: action.title },
+      };
+    }
+    case SET_INFO_SUP_VALUE: {
+      return {
+        ...state,
+        infoSupToAdd: { ...state.infoSupToAdd, value: action.value },
+      };
+    }
+    case ADD_INFO_SUP: {
+      return {
+        ...state,
+        infosSup: [
+          ...state.infosSup,
+          { title: action.info.title, value: action.info.value },
+        ],
+      };
+    }
+    case CLEAR_INFO_SUP: {
+      return {
+        ...state,
+        infoSupToAdd: { title: '', value: '' },
+      };
+    }
+    default: {
+      return state;
     }
   }
 };
